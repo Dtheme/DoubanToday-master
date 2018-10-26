edda16fbfb4202c08742c7520ccff822e80e9d06
duanzhiwei <armchannel_dzw@163.com>
2018-10-26 17:23:57 +0800

    update

---
+++ Douban-today/Utils/DzwSingleton.h
@@ -0,0 +1,18 @@
+//
+//  DzwSingleton.h
+//  Douban-today
+//
+//  Created by dzw on 2018/10/26.
+//  Copyright © 2018 dzw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MainTabbarViewController.h"
+@interface DzwSingleton : NSObject
+
+@property (nonatomic, assign) BOOL kIsTouchIDVerification;
+@property (nonatomic, strong) MainTabbarViewController *tabBarVC;
+@property (nonatomic, copy) NSString *currentCity;
++ (DzwSingleton *)sharedInstance;
+
+@end
